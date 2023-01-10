9a05a3468318ce861b9605e6c49c6c7188948887
Anthony P <43166263+anthonypz@users.noreply.github.com>
2023-01-10 13:43:37 -0800

    Create helper functions for data fetching

---
+++ client/src/services/makeRequest.ts
@@ -0,0 +1,15 @@
+import axios, { AxiosRequestConfig } from 'axios'
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_SERVER_URL,
+  withCredentials: true,
+})
+
+export function makeRequest(
+  url: string,
+  options?: AxiosRequestConfig<any> | undefined
+) {
+  return api(url, options)
+    .then((res) => res.data)
+    .catch((error) => Promise.reject(error?.response?.data?.message ?? 'Error'))
+}
